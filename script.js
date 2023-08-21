81745d36250d49b7b8f6d0d8bf4c3858c5287747
manish tiwari <manishtiwari769@gmail.com>
2023-08-21 18:23:42 +0530

    Commited

--- script.js
+++ script.js
@@ -13,8 +13,23 @@ function readTextFile(file, callback){
     rawFile.send(null);
 }
 
-readTextFile("C:\Users\manis\OneDrive\Desktop\tv_show.json", function(text){
-    var data = JSON.stringify(text);
-    // var data = JSON.parse(text);
-    console.log(data);
+readTextFile("/OTT_Task/tv_show.json", function(text){
+    var data = JSON.parse(text);
+    console.log(data)
+    data.forEach(element => {
+        return element;
+    }).map((element)=>{
+        console.log(element.id);
+        var name = element.name;
+        var airDate = element.airdate;
+        var airTime = element.airtime;
+        var img = element.img;
+        var rating = element.rating.average;
+        var runTime = element.runtime;
+        var season = element.season;
+        var type = element.type;
+        var url = element.url;
+        var id = element.id;
+    });
+    
 })
