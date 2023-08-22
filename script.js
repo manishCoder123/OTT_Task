2bcbe8394a815618bc8afbbcb36bcb3c76832ff9
manish tiwari <manishtiwari769@gmail.com>
2023-08-23 00:08:01 +0530

    problem not resolved

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-function readTextFile(file, textListner)
-{
+function readTextFile(file, textListner) {
     var rawFile = new XMLHttpRequest();
     rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
+    rawFile.onreadystatechange = function () {
+        if (rawFile.readyState === 4) {
+            if (rawFile.status === 200 || rawFile.status == 0) {
                 var allText = rawFile.responseText;
                 textListner(allText);
             }
@@ -16,33 +12,82 @@ function readTextFile(file, textListner)
     rawFile.send(null);
 }
 
-readTextFile("/OTT_Task/tv_show.json", function(text){
+readTextFile("/OTT_Task/tv_show.json", function (text) {
     var data = JSON.parse(text);
-    console.log(data)
+    // console.log(data)
 
     // crete array
     var tv_show_list = [];
-
+    var flag = 0;
     data.slice(0, 10).forEach(element => {
-        console.log(element.id);
+
         var name = element.name;
         var airDate = element.airdate;
         var airTime = element.airtime;
-        var img = element.img;
-        var rating = element.rating.average;
         var runTime = element.runtime;
         var season = element.season;
         var type = element.type;
         var url = element.url;
         var id = element.id;
+        var img = element._embedded.show.image.medium;
 
         var tv_show = {
-            "name" : name
-        }; 
+            "Name": name,
+            "Air_Date": airDate,
+            "Air_Time": airTime,
+            "Run_Time": runTime,
+            "Season": season,
+            "Type": type,
+            "URL": url,
+            "ID": id,
+            "Image": img
+        };
+        var node = document.getElementById("node");
+        const clone = node.cloneNode(true);
+        clone.setAttribute("id", "node-" + (flag + 1));
+        document.getElementById("card-div").appendChild(clone);
+        console.log("tv_show.Image-->" + (flag + 1), tv_show.Image)
+        if (tv_show.Image != null) {
+            document.querySelector("#img").src = tv_show.Image;
 
-
-        tv_show_list.push(tv_show);
+        } else {
+            document.querySelector("#img").src = "https://nwamotherlode.com/wp-content/uploads/2011/06/movie-graphic-placeholder.jpg";
+        }
+        flag++;
     });
+});
 
-    
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Other way to fetch Jason Data
+
+// fetch("tv_show.json").then(apidata=>{
+//     return apidata.json()
+// }).then((jsondata) => {
+//     jsondata.map((element)=>{
+//         id = element.id;
+//         let img = document.createElement("img");
+//         img.src =element._embedded.show.image.medium;
+//         document.getElementById("card-div").appendChild(img);
+//         console.log("json", id)
+//     });
+// });
