acfeae1a445752ca6f0cf90abdbcd0fa38b730df
manish tiwari <manishtiwari769@gmail.com>
2023-08-25 22:41:21 +0530

    added

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ function addCard(element, flag) {
         "ID": id,
         "Image": img
     };
-
+    
     var node = document.getElementById("node");
     
     var clone = node.cloneNode(true);
@@ -62,17 +62,14 @@ function addCard(element, flag) {
     if (tv_show.Image != null) {
         document.querySelector(getCurrentIndex(flag) + " > #img").src = tv_show.Image;
 
-    } else {
-        document.querySelector(getCurrentIndex(flag) + " > #img").src = "https://nwamotherlode.com/wp-content/uploads/2011/06/movie-graphic-placeholder.jpg";
-    }
+    } 
+    // else {
+    //     document.querySelector(getCurrentIndex(flag) + " > #img").src = "https://nwamotherlode.com/wp-content/uploads/2011/06/movie-graphic-placeholder.jpg";
+    // }
 }
 
 function getCurrentIndex(index) {
-    if (index == 0) {
-        return "#node";
-    } else {
-        return "#node_" + (index);
-    }
+    return index == 0 ? "#node" : ("#node-" + index);
 }
 
 
